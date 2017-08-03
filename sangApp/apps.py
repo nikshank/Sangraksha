bb523aa2ee8d7275ce223f7034ca34a13a8a09cf
nikshank <nshanker@asu.edu>
2017-08-03 04:04:04 +0000

    new changes added

---
+++ sangApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SangappConfig(AppConfig):
+    name = 'sangApp'
--- sangraksha/settings.py
+++ sangraksha/settings.py
@@ -76,7 +76,12 @@ WSGI_APPLICATION = 'sangraksha.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/sangDB/sangraksha.db',
+#	'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+	'USER': '',
+	'PASSWORD': '',
+	'HOST': '',
+	'PORT': '',
     }
 }
 
